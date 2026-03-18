c41326ff1cf597cf892518ec78af8aaa19c86335
halemmahhadi <halhadi000@gmail.com>
2026-03-18 12:26:24 +0100

    fixed some issues

--- backend/src/main/java/org/example/app/task/logic/mapper/TaskItemMapper.java
+++ backend/src/main/java/org/example/app/task/logic/mapper/TaskItemMapper.java
@@ -1,10 +1,12 @@
 package org.example.app.task.logic.mapper;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import org.example.app.task.dataaccess.TaskItemEntity;
 import org.example.app.task.logic.etos.TaskItemEto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Named;
 
-@Mapper
+@Mapper(componentModel = "jakarta")
 public interface TaskItemMapper {
 
   TaskItemEto toEto(TaskItemEntity entity);
--- backend/src/main/java/org/example/app/task/logic/mapper/TaskListMapper.java
+++ backend/src/main/java/org/example/app/task/logic/mapper/TaskListMapper.java
@@ -1,10 +1,12 @@
 package org.example.app.task.logic.mapper;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import org.example.app.task.dataaccess.TaskListEntity;
 import org.example.app.task.logic.etos.TaskListEto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Named;
 
-@Mapper
+@Mapper(componentModel = "jakarta")
 public interface TaskListMapper {
 
   TaskListEto toEto(TaskListEntity entity);
