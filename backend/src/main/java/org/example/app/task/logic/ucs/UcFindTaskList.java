c03733e7cd7c74d99f823030fea4f81bf76758ce
Salim Bouchara <salim.bouchara@capgemini.com>
2026-03-18 11:17:15 +0100

    added ucs

---
+++ backend/src/main/java/org/example/app/task/logic/ucs/UcFindTaskList.java
@@ -0,0 +1,12 @@
+package org.example.app.task.logic.ucs;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+import jakarta.transaction.Transactional;
+
+@ApplicationScoped
+@Named
+@Transactional
+public class UcFindTaskList {
+
+}
